38827ce726119b915ab599397eef14a4989ef64d
fbiernat <filipbiernat@gmail.com>
2020-02-10 21:20:52 +0100

    Initial commit

---
+++ src/test/java/pl/firma/projekt/carbase/CarbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.firma.projekt.carbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
