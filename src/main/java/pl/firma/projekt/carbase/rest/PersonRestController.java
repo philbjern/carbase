3beb96ad884475f9ad330225770111d798d0614c
fbiernat <filipbiernat@gmail.com>
2020-02-11 16:44:38 +0100

    Add rest of basic CRUD endpoints

--- src/main/java/pl/firma/projekt/carbase/rest/PersonRestController.java
+++ src/main/java/pl/firma/projekt/carbase/rest/PersonRestController.java
@@ -1,9 +1,8 @@
 package pl.firma.projekt.carbase.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.firma.projekt.carbase.entity.Car;
 import pl.firma.projekt.carbase.entity.Person;
 import pl.firma.projekt.carbase.service.PersonService;
 
@@ -23,6 +22,50 @@ public class PersonRestController {
     @GetMapping("/persons")
     public List<Person> findAll() {
         return personService.findAll();
+    }
+
+    @GetMapping("/persons/{personId}")
+    public Person getPerson(@PathVariable int personId) {
+        Person person = personService.findById(personId);
+        if (person == null) {
+            throw new RuntimeException("Person id not found " + personId);
+        }
+        return person;
+    }
+
+    @GetMapping("/persons/{personId}/cars")
+    public List<Car> getPersonCars(@PathVariable int personId) {
+        Person person = personService.findById(personId);
+        if (person == null) {
+            throw new RuntimeException("Person id not found " + personId);
+        }
+        List<Car> cars = person.getCars();
+        return cars;
+    }
+
+    @PostMapping("/persons")
+    public Person addPerson(@RequestBody Person person) {
+        // also just in case they pass an id in JSON... set id to 0
+        // to force a save of new item... instead of update
+        person.setId(0);
+        personService.save(person);
+        return person;
+    }
+
+    @PutMapping("/persons")
+    public Person updatePerson(@RequestBody Person person) {
+        personService.save(person);
+        return person;
+    }
+
+    @DeleteMapping("/person/{personId}")
+    public String deletePerson(@PathVariable int personId) {
+        Person person = personService.findById(personId);
+        if (person == null) {
+            throw new RuntimeException("Person id not found " + personId);
+        }
+        personService.deleteById(personId);
+        return "Deleted person id " + personId;
     }
 
 }
