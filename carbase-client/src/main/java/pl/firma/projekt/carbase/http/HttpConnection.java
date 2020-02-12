93f7c2c6810c54b48f06978333a308ac412fec27
fbiernat <filipbiernat@gmail.com>
2020-02-12 23:06:16 +0100

    Refactor class to contain only two methods

--- carbase-client/src/main/java/pl/firma/projekt/carbase/http/HttpConnection.java
+++ carbase-client/src/main/java/pl/firma/projekt/carbase/http/HttpConnection.java
@@ -6,73 +6,54 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class HttpConnection {
 
-    public String get(String urlString) {
-        if (urlString.equals(""))
-            throw new IllegalArgumentException("URL is required");
-
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json; utf-8");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-
-            int status = connection.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            connection.disconnect();
-            return content.toString();
-
-        } catch (IOException e) {
-            System.out.println("Cannot connect to " + urlString + ", " + e.getMessage());
+    public String request(String urlString, String method, String json) {
+        if (urlString.equals("") || method.equals("")) {
+            throw new IllegalArgumentException("URL and request method are required");
         }
-        return "";
-    }
-
-    public String post(String urlString, String json) {
-        if (urlString.equals("") || json.equals("")) {
-            throw new IllegalArgumentException("URL and request body are required");
+        if (json != null && json.isEmpty() && (method.equalsIgnoreCase("POST") || method.equalsIgnoreCase("PUT"))) {
+            throw new IllegalArgumentException("Request body is required when using POST or PUT method");
         }
 
         try {
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
+            connection.setRequestMethod(method);
             connection.setRequestProperty("Content-Type", "application/json; utf-8");
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setDoOutput(true);
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
 
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = json.getBytes("utf-8");
-                os.write(input, 0, input.length);
+            if (json != null && !json.isEmpty()) {
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
+                try (OutputStream os = connection.getOutputStream()) {
+                    byte[] input = json.getBytes(StandardCharsets.UTF_8);
+                    os.write(input, 0, input.length);
+                }
             }
 
             // Read the response from input stream
             try (BufferedReader br = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+                    new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
                 StringBuilder response = new StringBuilder();
                 String responseLine = null;
                 while ((responseLine = br.readLine()) != null) {
                     response.append(responseLine.trim());
                 }
+                connection.disconnect();
                 return response.toString();
             }
 
         } catch (IOException e) {
-            System.out.println("Cannot connect to " + urlString + ", " + e.getMessage());
+            return "Error connecting to " + urlString + ": " + e.getMessage();
         }
-        return "";
+    }
+
+    public String request(String url, String method) {
+        return this.request(url, method, "");
     }
 
 }
