06f3f7441f78273cbeef21314de8d5e52d0165cc
fbiernat <filipbiernat@gmail.com>
2020-02-12 16:06:41 +0100

    Refactor request method into two separate methods for GET requests and POST requests

--- carbase-client/src/main/java/pl/firma/projekt/carbase/http/HttpConnection.java
+++ carbase-client/src/main/java/pl/firma/projekt/carbase/http/HttpConnection.java
@@ -3,20 +3,21 @@ package pl.firma.projekt.carbase.http;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpConnection {
 
-    public String request(String urlString, String method) {
-        if (urlString.equals("") || method.equals(""))
-            throw new IllegalArgumentException("URL and method are required");
+    public String get(String urlString) {
+        if (urlString.equals(""))
+            throw new IllegalArgumentException("URL is required");
 
         try {
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod(method);
-            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
 
@@ -30,6 +31,43 @@ public class HttpConnection {
             in.close();
             connection.disconnect();
             return content.toString();
+
+        } catch (IOException e) {
+            System.out.println("Cannot connect to " + urlString + ", " + e.getMessage());
+        }
+        return "";
+    }
+
+    public String post(String urlString, String json) {
+        if (urlString.equals("") || json.equals("")) {
+            throw new IllegalArgumentException("URL and request body are required");
+        }
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = json.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // Read the response from input stream
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                return response.toString();
+            }
 
         } catch (IOException e) {
             System.out.println("Cannot connect to " + urlString + ", " + e.getMessage());
