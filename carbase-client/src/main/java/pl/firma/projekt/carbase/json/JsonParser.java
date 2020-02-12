98c79b55065570301fae63ec1170fd0e62e9c63f
fbiernat <filipbiernat@gmail.com>
2020-02-12 23:08:53 +0100

    Add and rename existing ultility methods

--- carbase-client/src/main/java/pl/firma/projekt/carbase/json/JsonParser.java
+++ carbase-client/src/main/java/pl/firma/projekt/carbase/json/JsonParser.java
@@ -3,6 +3,7 @@ package pl.firma.projekt.carbase.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.firma.projekt.carbase.entity.Car;
 import pl.firma.projekt.carbase.entity.Person;
 
 import java.util.List;
@@ -23,13 +24,21 @@ public class JsonParser {
     public String stringifyPretty(String response) throws JsonProcessingException {
         Object json = objectMapper.readValue(response, Object.class);
         String jsonPretty = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
-
         return jsonPretty;
     }
 
-    public List<Person> parsePersonList(String jsonString) throws JsonProcessingException {
+    public String stringifyPretty(Object obj) throws JsonProcessingException {
+        String jsonPretty = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        return jsonPretty;
+    }
+
+    public List<Person> getPersonList(String jsonString) throws JsonProcessingException {
         List<Person> result = objectMapper.readValue(jsonString, new TypeReference<List<Person>>() {});
         return result;
     }
 
+    public List<Car> getCarList(String jsonString) throws JsonProcessingException {
+        List<Car> result = objectMapper.readValue(jsonString, new TypeReference<List<Car>>() {});
+        return result;
+    }
 }
