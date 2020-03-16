9da927bc5d908062019963540666d4429b2c6ef7
fbiernat <filipbiernat@gmail.com>
2020-03-16 18:31:41 +0100

    Configure vue-router and vue-resource

--- carbase-vue/src/main.js
+++ carbase-vue/src/main.js
@@ -1,7 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VueResource from "vue-resource";
+import App from "./App.vue";
+import { routes } from "./routes";
+import { config } from "./config";
+
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
+
+Vue.use(VueResource);
+Vue.http.options.root = config.apiBaseUrl;
 
 new Vue({
-  el: '#app',
+  el: "#app",
+  router,
   render: h => h(App)
-})
+});
