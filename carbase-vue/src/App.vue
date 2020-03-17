ffbccfd8645e8fc297eaf0683f4a4f8939194127
fbiernat <filipbiernat@gmail.com>
2020-03-17 15:45:22 +0100

    Add footer

--- carbase-vue/src/App.vue
+++ carbase-vue/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <app-header></app-header>
-    <app-notifications
-      :notifications="notifications"
-      @notificationremoved="removeNotification($event)"
-    ></app-notifications>
-    <transition name="fade" mode="out-in">
-      <router-view @notify="notify($event)"></router-view>
-    </transition>
+    <div class="app-body">
+      <app-header></app-header>
+      <app-notifications
+        :notifications="notifications"
+        @notificationremoved="removeNotification($event)"
+      ></app-notifications>
+      <transition name="fade" mode="out-in">
+        <router-view @notify="notify($event)"></router-view>
+      </transition>
+    </div>
+    <footer class="footer">
+      <p class="text-center">Carbase &copy; {{ new Date().getFullYear() }}</p>
+    </footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Notifications from "./components/Notifications.vue";
+
+const NOTIFICATION_TIMEOUT = 4000;
 
 export default {
   data() {
@@ -40,6 +47,10 @@ export default {
     },
     notify(notification) {
       this.notifications.push(notification);
+      // const id = this.notifications.length - 1;
+      // setTimeout(() => {
+      //   this.removeNotification(id);
+      // }, NOTIFICATION_TIMEOUT);
     }
   }
 };
@@ -71,19 +82,24 @@ p {
   line-height: 1.5rem;
 }
 
+.app-body {
+  min-height: 95vh;
+}
+
 .row {
   width: 100%;
 }
 
 .content-wrapper {
   background: rgba(255, 255, 255, 0.8);
-  border-radius: 5px;
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.05);
   padding: 1rem 2rem;
 }
 
 .container {
   max-width: 1200px;
-  min-width: 500px;
+  min-width: 850px;
   margin: auto;
 }
 
@@ -104,7 +120,9 @@ p {
 }
 
 .btn {
+  color: #c21f40;
   background: #f9dc5c;
+  font-size: 1rem;
   padding: 0.75rem 1rem;
   border: 0;
   border-radius: 5px;
@@ -114,6 +132,7 @@ p {
 }
 
 .btn:hover {
+  background: rgba(0, 0, 0, 0.12);
   color: #ed254e;
 }
 
@@ -133,38 +152,65 @@ p {
   transition: opacity 0.5s;
 }
 
-table {
+.table {
   width: 100%;
   border-spacing: 0;
   margin: 1rem 0;
 }
 
-thead td {
+.table thead td {
   font-weight: bold;
+  background: #fff;
 }
 
-tr:nth-child(even) {
-  background-color: rgba(0, 0, 0, 0.05);
+.table tr:nth-child(odd) {
+  background-color: #fff8d9;
 }
 
-td {
+.table td {
   text-align: center;
   width: auto;
   padding: 1rem;
   border-bottom: 1px solid rgba(0, 0, 0, 0.15);
 }
 
-td ul {
+.table td ul {
   list-style: none;
   margin: 0;
   padding: 0;
 }
 
-td ul li {
+.table td ul li {
   padding: 0.25rem;
 }
 
 a {
   color: inherit;
+}
+
+.footer {
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 0.8rem;
+  padding: 1rem;
+}
+
+.narrow {
+  padding: 0 13rem;
+}
+
+.narrow p {
+  line-height: 2rem;
+  text-align: justify;
+}
+
+.mt-3 {
+  margin-top: 3rem;
+}
+
+.message {
+  text-align: center;
+  padding: 5rem 0;
+  font-size: 2.5rem;
+  color: rgba(0, 0, 0, 0.15);
 }
 </style>
