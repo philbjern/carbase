08324dad61495b828f46cc7e5d3f9fead5071f7f
fbiernat <filipbiernat@gmail.com>
2020-03-17 15:45:55 +0100

    Add about section and changes in styling

--- carbase-vue/src/components/Home.vue
+++ carbase-vue/src/components/Home.vue
@@ -20,6 +20,35 @@
         </div>
       </router-link>
     </div>
+
+    <h1 class="text-center mt-3">About Carbase</h1>
+    <div class="narrow">
+      <i class="fa fa-road car-bg"></i>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed
+        faucibus sem. Mauris quis neque vestibulum, ullamcorper enim nec, ornare
+        ligula. Vestibulum convallis elit ac tortor ullamcorper lacinia. Sed
+        facilisis nibh enim, at commodo dolor pharetra at. Vivamus a metus
+        euismod, molestie risus sed, scelerisque eros. Suspendisse potenti.
+        Morbi aliquam viverra risus mollis accumsan. Pellentesque sit amet
+        dignissim nisl, eget lacinia ligula. Nunc euismod pulvinar lobortis.
+        Curabitur et semper ex, vitae fermentum odio. Nulla facilisi. Fusce ut
+        diam quis nunc porta faucibus. Suspendisse enim justo, iaculis ut purus
+        eget, tincidunt mattis urna. Proin scelerisque venenatis commodo. Cras
+        at imperdiet felis, vitae elementum enim.
+      </p>
+
+      <p>
+        Aliquam nisi dui, congue vitae lacus sit amet, eleifend finibus velit.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Aliquam nibh massa, tincidunt at lectus sit amet,
+        malesuada ornare mauris. Nunc aliquet, ante at hendrerit accumsan, est
+        purus gravida felis, imperdiet vehicula purus justo nec erat. Aenean
+        arcu orci, lobortis in lorem eget, malesuada placerat quam. Mauris
+        dapibus consectetur feugiat. Maecenas tincidunt aliquam libero ac
+        commodo.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -54,7 +83,7 @@ export default {};
 .card:hover {
   background: black;
   transform: translateY(-15px);
-  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.75);
+  box-shadow: 5px 15px 15px rgba(0, 0, 0, 0.55);
 }
 
 .card .content {
@@ -69,5 +98,18 @@ export default {};
 
 .content p {
   color: inherit;
+}
+
+.narrow {
+  position: relative;
+}
+
+.car-bg {
+  font-size: 7rem;
+  color: rgba(0, 0, 0, 0.85);
+  position: absolute;
+  left: 3rem;
+  top: -5rem;
+  transform: rotate(25deg);
 }
 </style>
