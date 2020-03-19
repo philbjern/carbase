13eb8ee7f6aa7b1b3f2cfb507729abd1e2488fb8
fbiernat <filipbiernat@gmail.com>
2020-03-19 17:49:56 +0100

    Add route for editing and adding new person forms

--- carbase-vue/src/routes.js
+++ carbase-vue/src/routes.js
@@ -1,9 +1,12 @@
 import Home from "./components/Home.vue";
 import PersonList from "./components/persons/PersonList.vue";
+import PersonForm from "./components/persons/PersonForm.vue";
 import CarList from "./components/cars/CarList.vue";
 
 export const routes = [
   { path: "/", component: Home },
   { path: "/persons", component: PersonList },
+  { path: "/persons/new", component: PersonForm },
+  { path: "/persons/:id/edit", component: PersonForm },
   { path: "/cars", component: CarList }
 ];
