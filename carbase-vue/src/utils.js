e44f669b91b857255dff17608fa0c0d4487d7616
fbiernat <filipbiernat@gmail.com>
2020-03-19 17:50:21 +0100

    Add contains() and scrollTop() utility functions

--- carbase-vue/src/utils.js
+++ carbase-vue/src/utils.js
@@ -4,3 +4,19 @@ export function getNotification(type, msg) {
     message: msg
   };
 }
+
+export function contains(arr, id) {
+  let contains = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id == id) {
+      contains = true;
+      break;
+    }
+  }
+  return contains;
+}
+
+export function scrollTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
