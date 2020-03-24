47b6e08a625625f145827bb8b2e689a18d7924bc
fbiernat <filipbiernat@gmail.com>
2020-03-24 17:52:40 +0100

    Add new routes and change routes for editing entities

--- carbase-vue/src/routes.js
+++ carbase-vue/src/routes.js
@@ -2,11 +2,14 @@ import Home from "./components/Home.vue";
 import PersonList from "./components/persons/PersonList.vue";
 import PersonForm from "./components/persons/PersonForm.vue";
 import CarList from "./components/cars/CarList.vue";
+import CarForm from "./components/cars/CarForm.vue";
 
 export const routes = [
   { path: "/", component: Home },
   { path: "/persons", component: PersonList },
   { path: "/persons/new", component: PersonForm },
-  { path: "/persons/:id/edit", component: PersonForm },
-  { path: "/cars", component: CarList }
+  { path: "/persons/:id", component: PersonForm },
+  { path: "/cars", component: CarList },
+  { path: "/cars/new", component: CarForm },
+  { path: "/cars/:id", component: CarForm }
 ];
