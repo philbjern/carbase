2d50928489a02aad9fe38820ce3a4b6826bc9f61
fbiernat <filipbiernat@gmail.com>
2020-05-25 16:31:52 +0200

    Grant privilages for new database

--- sql/createuser.sql
+++ sql/createuser.sql
@@ -3,4 +3,5 @@ DROP USER 'carbaseadmin'@'localhost';
 CREATE USER 'carbaseadmin'@'localhost' IDENTIFIED BY 'carbaseadmin';
 GRANT ALL PRIVILEGES ON persondb.* TO 'carbaseadmin'@'localhost';
 GRANT ALL PRIVILEGES ON cardb.* TO 'carbaseadmin'@'localhost';
+GRANT ALL PRIVILEGES ON carsharingdb.* TO 'carbaseadmin'@'localhost';
 GRANT ALL PRIVILEGES ON carbase.* TO 'carbaseadmin'@'localhost';
