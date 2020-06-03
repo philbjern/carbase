548b419cab8fa6ef851027a79dfdde5e26f1aa09
fbiernat <filipbiernat@gmail.com>
2020-06-03 15:15:51 +0200

    Fix mapping path

--- person-service/src/main/java/pl/carsoft/carbase/controller/PersonController.java
+++ person-service/src/main/java/pl/carsoft/carbase/controller/PersonController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/person/")
+@RequestMapping(path = "/api/person")
 
 public class PersonController {
 
