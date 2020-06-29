222bcc6ca41cb8e29099d4ab65a7fe2076d2cc84
fbiernat <filipbiernat@gmail.com>
2020-06-29 22:23:39 +0200

    Add methods for adding and updating cars, add equals and hashCode methods

--- api-gateway/src/main/java/pl/carsoft/carbase/gateway/entity/Person.java
+++ api-gateway/src/main/java/pl/carsoft/carbase/gateway/entity/Person.java
@@ -2,6 +2,7 @@ package pl.carsoft.carbase.gateway.entity;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class Person {
 
@@ -78,6 +79,19 @@ public class Person {
         this.carsList = carsList;
     }
 
+    public void addCar(Car car) {
+        this.carsList.add(car);
+    }
+
+    public void updateCar(Car car) {
+        for (int i = 0; i < carsList.size(); i++) {
+            if (carsList.get(i).getId().equals(car.getId())) {
+                carsList.remove(i);
+                carsList.add(i, car);
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return "Person{" +
@@ -86,116 +100,24 @@ public class Person {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", registeredOn=" + registeredOn +
+                ", carsList=" + carsList +
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName) &&
+                Objects.equals(email, person.email) &&
+                Objects.equals(registeredOn, person.registeredOn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, registeredOn);
+    }
+
 }
-
-
-//
-//@Entity
-//@Table(name="person")
-//public class Person {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id")
-//    private int id;
-//
-//    @Column(name = "first_name")
-//    private String firstName;
-//
-//    @Column(name = "last_name")
-//    private String lastName;
-//
-//    @Column(name = "email")
-//    private String email;
-//
-//    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
-//    @JoinTable(
-//            name = "person_car",
-//            joinColumns = @JoinColumn(name = "person_id"),
-//            inverseJoinColumns = @JoinColumn(name = "car_id")
-//    )
-//    private List<Car> cars;
-//
-//    public Person() {}
-//
-//    public Person(String firstName, String lastName, String email) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public List<Car> getCars() {
-//        return cars;
-//    }
-//
-//    public void setCars(List<Car> cars) {
-//        this.cars = cars;
-//    }
-//
-//    public List<Car> addCar(Car car) {
-//        cars.add(car);
-//        return cars;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Person{" +
-//                "id=" + id +
-//                ", firstName='" + firstName + '\'' +
-//                ", lastName='" + lastName + '\'' +
-//                ", email='" + email + '\'' +
-//                '}';
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Person person = (Person) o;
-//        return id == person.id &&
-//                Objects.equals(firstName, person.firstName) &&
-//                Objects.equals(lastName, person.lastName) &&
-//                Objects.equals(email, person.email) &&
-//                Objects.equals(cars, person.cars);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, firstName, lastName, email, cars);
-//    }
-//
-//}
