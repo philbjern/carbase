2e6aec6918f76eb64d8672135c30d50787b41def
fbiernat <filipbiernat@gmail.com>
2020-08-26 14:54:41 +0200

    Add docker support and build scripts

--- sql/createuser.sql
+++ sql/createuser.sql
@@ -1,7 +1,7 @@
-DROP USER 'carbaseadmin'@'localhost';
+DROP USER 'carbaseadmin'@'%';
 
-CREATE USER 'carbaseadmin'@'localhost' IDENTIFIED BY 'carbaseadmin';
-GRANT ALL PRIVILEGES ON persondb.* TO 'carbaseadmin'@'localhost';
-GRANT ALL PRIVILEGES ON cardb.* TO 'carbaseadmin'@'localhost';
-GRANT ALL PRIVILEGES ON carsharingdb.* TO 'carbaseadmin'@'localhost';
-GRANT ALL PRIVILEGES ON carbase.* TO 'carbaseadmin'@'localhost';
+CREATE USER 'carbaseadmin'@'%' IDENTIFIED BY 'carbaseadmin';
+GRANT ALL PRIVILEGES ON persondb.* TO 'carbaseadmin'@'%';
+GRANT ALL PRIVILEGES ON cardb.* TO 'carbaseadmin'@'%';
+GRANT ALL PRIVILEGES ON carsharingdb.* TO 'carbaseadmin'@'%';
+GRANT ALL PRIVILEGES ON carbase.* TO 'carbaseadmin'@'%';
