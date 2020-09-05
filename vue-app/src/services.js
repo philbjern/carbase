82fb337673d0017481000d92dfc51a726b14f6e3
fbiernat <filipbiernat@gmail.com>
2020-09-05 14:31:36 +0200

    Add Services object with services URLs

---
+++ vue-app/src/services.js
@@ -0,0 +1,6 @@
+export const Services = {
+  PERSON_SERVICE_URL: "http://person-service:8081",
+  CAR_SERVICE_URL: "http://car-service:8082",
+  CARSHARING_SERVICE_URL: "http://carsharing-service:8083",
+  APIGATEWAY_SERVICE_URL: "http://localhost:8084"
+};
