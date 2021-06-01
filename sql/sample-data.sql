3920bd426a24b4fd58716901cc63e8c9cca53e47
fbiernat <filipbiernat@gmail.com>
2021-06-01 16:55:48 +0200

    Add sql file with sample data

---
+++ sql/sample-data.sql
@@ -0,0 +1,33 @@
+ use cardb;
+ INSERT INTO car VALUES(2, 'Toyota', 'Auris', '2003', '2.0', '1');
+ INSERT INTO car VALUES(18, 'Volvo', 'V40', '2001', '2.0', '0');
+ INSERT INTO car VALUES(19, 'BMW', '320', '2004', '2.0', '1');
+ INSERT INTO car VALUES(20, 'Peugeot', '307', '2001', '2.0', '0');
+ INSERT INTO car VALUES(21, 'Kia', 'Stinger', '2020', '2.0', '1');
+ INSERT INTO car VALUES(22, 'Ford', 'Focus', '2003', '2.0', '1');
+ INSERT INTO car VALUES(27, 'Renault', 'Clio', '2007', '1.6', '1');
+ INSERT INTO car VALUES(28, 'Ford', 'Mustang', '2010', '5.0', '1');
+ 
+ use persondb;
+ INSERT INTO person VALUES(4, 'Julia', 'Kwiatkowska', 'juliakwiatkowska@gmail.com', '2020-06-03 18:16:50');
+ INSERT INTO person VALUES(8, 'Karolina', 'Nowak', 'karolinanowak@onet.pl', '2020-06-05 13:17:56');
+ INSERT INTO person VALUES(28, 'Juliusz', 'Kowalski', 'juliuszkowalski@onet.pl', '2020-06-05 15:43:14');
+ INSERT INTO person VALUES(29, 'Anna', 'Borkowsky', 'annbork@interia.pl', '2020-06-22 13:35:14');
+ INSERT INTO person VALUES(30, 'Marcin', 'Zborowski', 'mzborowski@gmail.com', '2020-06-30 19:48:45');
+ 
+ use carsharingdb;
+ INSERT INTO carsharing VALUES(12, 20, 4);
+ INSERT INTO carsharing VALUES(13, 19, 4);
+ INSERT INTO carsharing VALUES(15, 22, 8);
+ INSERT INTO carsharing VALUES(17, 18, 8);
+ INSERT INTO carsharing VALUES(18, 19, 28);
+ INSERT INTO carsharing VALUES(19, 20, 8);
+ INSERT INTO carsharing VALUES(20, 21, 8);
+ INSERT INTO carsharing VALUES(21, 2, 8);
+ INSERT INTO carsharing VALUES(22, 19, 8);
+ INSERT INTO carsharing VALUES(23, 18, 29);
+ INSERT INTO carsharing VALUES(27, 27, 29);
+ 
+ 
+ 
+ 
